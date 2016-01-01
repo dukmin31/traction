f9df6d92e7aa944cc04fb4ae908fecdaac3ddead
Franze Jr <franzejr@gmail.com>
2016-01-01 10:12:48 -0300

    Small refactorings

--- tractionapi/app/serializers/website_serializer.rb
+++ tractionapi/app/serializers/website_serializer.rb
@@ -1,3 +1,3 @@
 class WebsiteSerializer < ActiveModel::Serializer
-  attributes :id, :rank, :url ,:page_views_per_user, :page_views_per_million
+  attributes :id, :rank, :url, :page_views_per_user, :page_views_per_million
 end
